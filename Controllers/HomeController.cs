4bbf53b4697571f6c498b2d27ab8f1a9fe3cc3d8
Ahmet Emin <buyukculhaci3@hotmail.com>
2019-10-05 21:07:26 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using GoldenPersonelYonetimi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoldenPersonelYonetimi.Controllers
+{
+    public class HomeController : Controller
+    {
+        //PersonelListesi_Result db = new PersonelListesi_Result();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("KProfil")]
+        public ActionResult UserProfile(int Id)
+        {
+            userManagementEntities db = new userManagementEntities();
+            var res = db.kullCek(Id).FirstOrDefault();
+            return View(res);
+        }
+
+        [Route("Liste")]
+        public ActionResult TableList()
+        {
+            userManagementEntities db = new userManagementEntities();
+            var res = db.PersonelListesi(1).ToList();
+            return View(res);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoldenPersonelYonetimi.MvcApplication" Language="C#" %>
